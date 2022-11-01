5f7fc142b0bb8bb6f6ba859aba5b0612d0706f60
Vladimir Tagle González <vladitg@hotmail.com>
2022-11-01 08:26:21 -0600

    Archvos faltantes de commit anterior

--- ClassLibrary/FindFarthestNode.cs
+++ ClassLibrary/FindFarthestNode.cs
@@ -7,25 +7,29 @@ namespace ClassLibrary
     {
         public FindFarthestNode(string strArr)
         {
-            char[] separators = new char[] { '[', ']', '"', ',' };
+            char[] separators = new char[] { '[', ']', '"', ',', ' ' };
             string[] caminos = strArr.Split(separators, StringSplitOptions.RemoveEmptyEntries);
             List<string> caminosLargos = new List<string>();
             int caminosMax = 0;
             string camino;
-            foreach(var aux in caminos)
+            foreach (var aux in caminos)
             {
                 string[] puntos = aux.Split('-');
                 string inicio = puntos[0];
                 string final = puntos[1];
-                string puente = puntos[1];
+                string puente = puntos[0];
                 camino = inicio + "-" + final;
                 int caminosCount = 1;
-                bool flag = true;
-                List<string> caminosLargosChild = new List<string>();
-                while (flag == true)
+
+                List<string> nodosRecorridos = new List<string>();
+                nodosRecorridos.Add(inicio);
+                nodosRecorridos.Add(final);
+                List<string> nodosCerrados = new List<string>();
+                int iNodo = 2;
+
+                bool flagRestart = true;
+                while (flagRestart == true)
                 {
-                    string caminoChild = camino;
-                    int caminosCountChild = 1;
                     foreach (var res in caminos)
                     {
                         string[] puntosChild = res.Split('-');
@@ -33,45 +37,54 @@ namespace ClassLibrary
                         string finalChild = puntosChild[1];
                         if (inicioChild == final)
                         {
-                            caminosCountChild++;
-                            caminoChild += "-" + finalChild;
-                            final = finalChild;
+                            if (!nodosCerrados.Contains(finalChild))
+                            {
+                                flagRestart = true;
+                                puente = final;
+                                caminosCount++;
+                                camino += "-" + finalChild;
+                                final = finalChild;
+                                nodosRecorridos.Add(final);
+                                iNodo++;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            flagRestart = false;
                         }
                     }
-                    if(puente != final)
+                    if (flagRestart == false)
                     {
-                        puente = final;
-                    }
-                    else
-                    {
-                        flag = false;
-                    }
-                    if (caminosCountChild > caminosCount)
-                    {
-                        caminosCount = caminosCountChild;
-                        caminosLargosChild = new List<string>();
-                        caminosLargosChild.Add(caminoChild);
-                    }
-                    else if (caminosCountChild == caminosCount)
-                    {
-                        caminosLargosChild.Add(caminoChild);
-                    }
-                }
-                if(caminosCount > caminosMax)
-                {
-                    caminosMax = caminosCount;
-                    caminosLargos = new List<string>();
-                    foreach(var child in caminosLargosChild)
-                    {
-                        caminosLargos.Add(child);
-                    }
-                    
-                }
-                else if (caminosCount == caminosMax)
-                {
-                    foreach (var child in caminosLargosChild)
-                    {
-                        caminosLargos.Add(child);
+                        if ((iNodo - 2) != -1)
+                        {
+                            nodosCerrados.Add(nodosRecorridos[iNodo - 1]);
+                            nodosRecorridos.RemoveAt(iNodo - 1);
+                            iNodo--;
+                            final = puente;
+                            if (iNodo - 2 != -1)
+                                puente = nodosRecorridos[iNodo - 2];
+                            if (caminosCount > caminosMax)
+                            {
+                                caminosMax = caminosCount;
+                                caminosLargos = new List<string>();
+                                caminosLargos.Add(camino);
+                            }
+                            else if (caminosCount == caminosMax)
+                            {
+                                caminosLargos.Add(camino);
+                            }
+                            caminosCount--;
+                            camino = "";
+                            for (var i = 0; i < iNodo; i++)
+                            {
+                                if (i == 0)
+                                    camino += nodosRecorridos[i];
+                                else
+                                    camino += "-" + nodosRecorridos[i];
+                            }
+                            flagRestart = true;
+                        }
                     }
                 }
             }
@@ -81,15 +94,19 @@ namespace ClassLibrary
                 string[] puntos = aux.Split('-');
                 string inicio = puntos[1];
                 string final = puntos[0];
-                string puente = puntos[0];
+                string puente = puntos[1];
                 camino = inicio + "-" + final;
                 int caminosCount = 1;
-                bool flag = true;
-                List<string> caminosLargosChild = new List<string>();
-                while (flag == true)
+
+                List<string> nodosRecorridos = new List<string>();
+                nodosRecorridos.Add(inicio);
+                nodosRecorridos.Add(final);
+                List<string> nodosCerrados = new List<string>();
+                int iNodo = 2;
+
+                bool flagRestart = true;
+                while (flagRestart == true)
                 {
-                    string caminoChild = camino;
-                    int caminosCountChild = 1;
                     foreach (var res in caminos)
                     {
                         string[] puntosChild = res.Split('-');
@@ -97,52 +114,64 @@ namespace ClassLibrary
                         string finalChild = puntosChild[0];
                         if (inicioChild == final)
                         {
-                            caminosCountChild++;
-                            caminoChild += "-" + finalChild;
-                            if (puente == final)
+                            if (!nodosCerrados.Contains(finalChild))
                             {
-                                caminosCountChild++;
-                                caminoChild += "-" + finalChild;
+                                flagRestart = true;
+                                puente = final;
+                                caminosCount++;
+                                camino += "-" + finalChild;
                                 final = finalChild;
+                                nodosRecorridos.Add(final);
+                                iNodo++;
+                                break;
                             }
-                            final = finalChild;
+                        }
+                        else
+                        {
+                            flagRestart = false;
                         }
                     }
-                    if (caminosCountChild > caminosCount)
+                    if (flagRestart == false)
                     {
-                        caminosCount = caminosCountChild;
-                        caminosLargosChild = new List<string>();
-                        caminosLargosChild.Add(caminoChild);
-                    }
-                    else if (caminosCountChild == caminosCount)
-                    {
-                        caminosLargosChild.Add(caminoChild);
-                    }
-                }
-                if (caminosCount > caminosMax)
-                {
-                    caminosMax = caminosCount;
-                    caminosLargos = new List<string>();
-                    foreach (var child in caminosLargosChild)
-                    {
-                        caminosLargos.Add(child);
-                    }
-
-                }
-                else if (caminosCount == caminosMax)
-                {
-                    foreach (var child in caminosLargosChild)
-                    {
-                        caminosLargos.Add(child);
+                        if ((iNodo - 2) != -1)
+                        {
+                            nodosCerrados.Add(nodosRecorridos[iNodo - 1]);
+                            nodosRecorridos.RemoveAt(iNodo - 1);
+                            iNodo--;
+                            final = puente;
+                            if (iNodo - 2 != -1)
+                                puente = nodosRecorridos[iNodo - 2];
+                            if (caminosCount > caminosMax)
+                            {
+                                caminosMax = caminosCount;
+                                caminosLargos = new List<string>();
+                                caminosLargos.Add(camino);
+                            }
+                            else if (caminosCount == caminosMax)
+                            {
+                                caminosLargos.Add(camino);
+                            }
+                            caminosCount--;
+                            camino = "";
+                            for (var i = 0; i < iNodo; i++)
+                            {
+                                if (i == 0)
+                                    camino += nodosRecorridos[i];
+                                else
+                                    camino += "-" + nodosRecorridos[i];
+                            }
+                            flagRestart = true;
+                        }
                     }
                 }
             }
 
-            Console.WriteLine("Se han encontrado los siguientes caminos");
-            foreach(var aux in caminosLargos)
+            Console.WriteLine("\nSe han encontrado los siguientes caminos");
+            foreach (var aux in caminosLargos)
             {
-                Console.WriteLine(aux);   
+                Console.WriteLine(aux);
             }
+            Console.WriteLine("\n");
         }
     }
 }
