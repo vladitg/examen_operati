21e990058d0e39b8717be8902de4c098038a2f03
Vladimir Tagle González <vladitg@hotmail.com>
2022-11-02 05:35:54 -0600

    Ajuste problema cuatro

--- ClassLibrary/FindFarthestNode.cs
+++ ClassLibrary/FindFarthestNode.cs
@@ -9,15 +9,42 @@ namespace ClassLibrary
         {
             char[] separators = new char[] { '[', ']', '"', ',', ' ' };
             string[] caminos = strArr.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-            List<string> caminosLargos = new List<string>();
-            int caminosMax = 0;
-            string camino;
+            List<string> arrFinal = new List<string>();
             foreach (var aux in caminos)
             {
                 string[] puntos = aux.Split('-');
-                string inicio = puntos[0];
-                string final = puntos[1];
-                string puente = puntos[0];
+                int comparison = String.Compare(puntos[0], puntos[1], comparisonType: StringComparison.OrdinalIgnoreCase);
+                string valorNew = "";
+                if (comparison < 0)
+                {
+                    arrFinal.Add(puntos[0] + "-" + puntos[1]);
+                }
+                else
+                {
+                    arrFinal.Add(puntos[1] + "-" + puntos[0]);
+                }
+            }
+
+            List<string> caminosLargos = new List<string>();
+            int caminosMax = 0;
+            string camino;
+            foreach (var aux in arrFinal)
+            {
+                string[] puntos = aux.Split('-');
+                int comparison = String.Compare(puntos[0], puntos[1], comparisonType: StringComparison.OrdinalIgnoreCase);
+                string inicio = "", final = "", puente = "";
+                if (comparison < 0)
+                {
+                    inicio = puntos[0];
+                    final = puntos[1];
+                    puente = puntos[0];
+                } else
+                {
+                    inicio = puntos[1];
+                    final = puntos[0];
+                    puente = puntos[1];
+                }
+
                 camino = inicio + "-" + final;
                 int caminosCount = 1;
 
@@ -30,7 +57,7 @@ namespace ClassLibrary
                 bool flagRestart = true;
                 while (flagRestart == true)
                 {
-                    foreach (var res in caminos)
+                    foreach (var res in arrFinal)
                     {
                         string[] puntosChild = res.Split('-');
                         string inicioChild = puntosChild[0];
@@ -64,17 +91,7 @@ namespace ClassLibrary
                             final = puente;
                             if (iNodo - 2 != -1)
                                 puente = nodosRecorridos[iNodo - 2];
-                            if (caminosCount > caminosMax)
-                            {
-                                caminosMax = caminosCount;
-                                caminosLargos = new List<string>();
-                                caminosLargos.Add(camino);
-                            }
-                            else if (caminosCount == caminosMax)
-                            {
-                                caminosLargos.Add(camino);
-                            }
-                            caminosCount--;
+                            caminosLargos.Add(camino);
                             camino = "";
                             for (var i = 0; i < iNodo; i++)
                             {
@@ -89,89 +106,24 @@ namespace ClassLibrary
                 }
             }
 
-            foreach (var aux in caminos)
+            caminosLargos.Sort((x, y) => x.Length.CompareTo(y.Length));
+            int index = caminosLargos.Count;
+            List<string> caminoMasLargo = new List<string>(caminosLargos[index - 1].Split('-'));
+            List<string> primerCamino = new List<string>(caminosLargos[index - 1].Split('-'));
+            List<string> segundoCamino = new List<string>(caminosLargos[index - 2].Split('-'));
+            foreach (var aux in caminoMasLargo)
             {
-                string[] puntos = aux.Split('-');
-                string inicio = puntos[1];
-                string final = puntos[0];
-                string puente = puntos[1];
-                camino = inicio + "-" + final;
-                int caminosCount = 1;
-
-                List<string> nodosRecorridos = new List<string>();
-                nodosRecorridos.Add(inicio);
-                nodosRecorridos.Add(final);
-                List<string> nodosCerrados = new List<string>();
-                int iNodo = 2;
-
-                bool flagRestart = true;
-                while (flagRestart == true)
-                {
-                    foreach (var res in caminos)
-                    {
-                        string[] puntosChild = res.Split('-');
-                        string inicioChild = puntosChild[1];
-                        string finalChild = puntosChild[0];
-                        if (inicioChild == final)
-                        {
-                            if (!nodosCerrados.Contains(finalChild))
-                            {
-                                flagRestart = true;
-                                puente = final;
-                                caminosCount++;
-                                camino += "-" + finalChild;
-                                final = finalChild;
-                                nodosRecorridos.Add(final);
-                                iNodo++;
-                                break;
-                            }
-                        }
-                        else
-                        {
-                            flagRestart = false;
-                        }
-                    }
-                    if (flagRestart == false)
-                    {
-                        if ((iNodo - 2) != -1)
-                        {
-                            nodosCerrados.Add(nodosRecorridos[iNodo - 1]);
-                            nodosRecorridos.RemoveAt(iNodo - 1);
-                            iNodo--;
-                            final = puente;
-                            if (iNodo - 2 != -1)
-                                puente = nodosRecorridos[iNodo - 2];
-                            if (caminosCount > caminosMax)
-                            {
-                                caminosMax = caminosCount;
-                                caminosLargos = new List<string>();
-                                caminosLargos.Add(camino);
-                            }
-                            else if (caminosCount == caminosMax)
-                            {
-                                caminosLargos.Add(camino);
-                            }
-                            caminosCount--;
-                            camino = "";
-                            for (var i = 0; i < iNodo; i++)
-                            {
-                                if (i == 0)
-                                    camino += nodosRecorridos[i];
-                                else
-                                    camino += "-" + nodosRecorridos[i];
-                            }
-                            flagRestart = true;
-                        }
-                    }
-                }
+                if (segundoCamino.Remove(aux))
+                    primerCamino.Remove(aux);
             }
+            int caminoUnido = primerCamino.Count + segundoCamino.Count + 1;
+            int response = 0;
+            if (caminoMasLargo.Count >= caminoUnido)
+                response = caminoMasLargo.Count - 1;
+            else
+                response = caminoUnido - 1;
 
-            Console.WriteLine("\nSe han encontrado los siguientes caminos");
-            foreach (var aux in caminosLargos)
-            {
-                Console.WriteLine(aux);
-            }
-            Console.WriteLine("\n");
+            Console.WriteLine("\nEl largo del camino es de: " + response + "\n");
         }
     }
 }
